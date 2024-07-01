59f2a0daf300210423caf8da1bf9d44dbae0b345
suraj <surajsuthar0067@gmail.com>
2024-07-01 22:08:36 +0530

    index.js

--- index.js
+++ index.js
@@ -5,11 +5,14 @@ const { Client } = require('pg');
 require('dotenv').config()
 
 const client = new Client({
-	user: 'suraj',
-	password: 'Suraj@0067',
-	host: 'localhost',
-	port: '5432',
-	database: 'aiportDB',
+	user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
+  database: process.env.DB_NAME,
+  ssl: {
+        rejectUnauthorized: false, // You might want to set this to true in production
+    }
 });
 
 
